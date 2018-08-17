065d0193807f514224de6bba1b778db531121029
Hosam <hossamnasser938@gmail.com>
2018-08-17 11:40:36 +0200

    Build a portion of app hierarchy

--- app/src/main/java/com/example/android/ocschat/activity/LoginActivity.kt
+++ app/src/main/java/com/example/android/ocschat/activity/LoginActivity.kt
@@ -1,12 +1,13 @@
-package com.example.android.ocschat
+package com.example.android.ocschat.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.android.ocschat.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
