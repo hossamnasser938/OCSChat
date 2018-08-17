4619f73e4082620809ef2c44514c8afd3e079663
Hosam <hossamnasser938@gmail.com>
2018-08-17 22:23:51 +0200

    Add menu to Home Activity containing sign out action

--- app/src/main/java/com/example/android/ocschat/activity/HomeActivity.kt
+++ app/src/main/java/com/example/android/ocschat/activity/HomeActivity.kt
@@ -3,6 +3,8 @@ package com.example.android.ocschat.activity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.example.android.ocschat.R
 import com.google.firebase.auth.FirebaseAuth
 
@@ -15,5 +17,21 @@ class HomeActivity : AppCompatActivity() {
             val intent = Intent(applicationContext, LoginActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.home_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId){
+            R.id.sign_out_menu_item -> {
+                //Sign user out and close app
+                FirebaseAuth.getInstance().signOut()
+                this.finishAffinity()
+            }
+        }
+        return true
     }
 }
