78ab1ddeb3e438353abb64c3d05d8bb39568c34a
Hosam <hossamnasser938@gmail.com>
2018-08-20 22:02:32 +0200

    Build initial AddFriend fragment

--- app/src/main/java/com/example/android/ocschat/activity/AddFriendActivity.kt
+++ app/src/main/java/com/example/android/ocschat/activity/AddFriendActivity.kt
@@ -2,12 +2,23 @@ package com.example.android.ocschat.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import com.example.android.ocschat.R
+import com.example.android.ocschat.fragment.AddFriendFragment
 
-class AddFriendActivity : AppCompatActivity() {
+class AddFriendActivity : AppCompatActivity() , AddFriendFragment.AddFriendTransitionInterface {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_friend)
+        openFragment(AddFriendFragment())
+    }
+
+    override fun openFragment(fragment: Fragment) {
+        supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.add_friend_frame_layout, fragment)
+                .addToBackStack(null)
+                .commit()
     }
 }
---
+++ app/src/main/java/com/example/android/ocschat/fragment/AddFriendFragment.kt
@@ -0,0 +1,28 @@
+package com.example.android.ocschat.fragment
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.android.ocschat.R
+
+class AddFriendFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_add_friend, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    interface AddFriendTransitionInterface{
+        fun openFragment(fragment: Fragment)
+    }
+
+}
